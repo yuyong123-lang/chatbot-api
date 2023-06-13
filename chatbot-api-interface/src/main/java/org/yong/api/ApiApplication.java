768506d01cff8a22754880e093ea35c9273081f2
yuyong <1372076088@qq.com>
2023-06-13 19:16:20 +0800

    init

---
+++ chatbot-api-interface/src/main/java/org/yong/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package org.yong.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dylan
+ * @date 2023/6/13 18:54
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
